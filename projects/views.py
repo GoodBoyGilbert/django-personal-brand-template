a699a987ff663341427acbd6691640f268e766c6
GoodBoyGilbert <johnmctighe2@gmail.com>
2024-09-08 20:18:49 -0500

    first commit, not complete yet

---
+++ projects/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+  return render(request, "index.html")
+
+def resume(request):
+  return render(request, "resume.html")
+
+def projects(request):
+  return render(request, "projects.html")
+
+def contact(request):
+  return render(request, "contact.html")
